87eece85a41f092658a46e2bfb4c47b997888509
DoveSkyger <sky-ger@live.com>
2023-09-02 20:52:40 +0800

    Add comments to make flycheck/flymake silent and adjust font settings

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
 ;;; early-init.el --- earliest birds               -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
 (setq load-prefer-newer t)
 
@@ -16,6 +18,25 @@
   (add-to-list 'package-archives
                (cons "melpa" "https://melpa.org/packages/")
                t))
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Set default font before frame creation to make sure the first frame have the correct size
+(defvar font-list
+  (cond
+   ((eq system-type 'darwin)
+    '(("Sarasa Mono SC Nerd Font" . 12) ("Monaco" . 12) ("Menlo" . 12)))
+   ((eq system-type 'windows-nt)
+    '(("Sarasa Mono SC Nerd Font" . 10) ("Consolas" . 11) ("Cascadia Mono" . 10)))
+   (t
+    '(("Sarasa Mono SC Nerd Font" . 10) ("Consolas" . 11) ("Cascadia Mono" . 10))))
+  "List of fonts and sizes.  The first one available will be used.")
+
+(add-to-list 'default-frame-alist (cons 'font (format "%s-%d" (caar font-list) (cdar font-list))))
+
 
 ;; Local Variables:
 ;; no-byte-compile: t
